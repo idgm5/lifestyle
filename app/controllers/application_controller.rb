9290af584c1e66e2faf3aaff941f5fcdbb3682d5
idgm5 <idgm@pm.me>
2020-05-18 18:07:24 -0500

    Add articles-row for latest post in CSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :user
+
+  private
+  def user
+    if session[:current_user_id].is_a? Integer
+       @user = User.find(session[:current_user_id])
+    else
+       @user = User.first
+    end
+  end
 end
