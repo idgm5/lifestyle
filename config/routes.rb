fa09d6ad635de60ec6675181b3450b2f697f6fb7
idgm5 <idgm@pm.me>
2020-05-22 11:20:04 -0500

    Add method/routes to create categories

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :articles
+  resources :categories
 
   root 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
